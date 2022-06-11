cd1c69d6d6ae7e99ac63a6b67ac50bc51017e498
ayushshrestha1892 <ayushshrestha96@gmail.com>
2022-06-11 14:16:11 +0545

    Login Screen UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,6 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
+
       //validator:
 
       onSaved: (value) {
@@ -27,12 +28,21 @@ class _LoginScreenState extends State<LoginScreen> {
       },
 
       textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.mail),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
     );
 
-    //email fields
+    //passwordfields
     final passwordField = TextFormField(
       autofocus: false,
       controller: passwordController,
+      obscureText: true,
 
       //validator:
 
@@ -41,6 +51,34 @@ class _LoginScreenState extends State<LoginScreen> {
       },
 
       textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.vpn_key),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {},
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
 
     return Scaffold(
@@ -49,13 +87,53 @@ class _LoginScreenState extends State<LoginScreen> {
           child: SingleChildScrollView(
         child: Container(
           color: Colors.white,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                emailField,
-                passwordField,
-              ],
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 140,
+                    child: Image.asset(
+                      "assets/logo.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  emailField,
+                  SizedBox(
+                    height: 25,
+                  ),
+                  passwordField,
+                  SizedBox(
+                    height: 35,
+                  ),
+                  loginButton,
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Don't have an account? "),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text("Signup",
+                            style: TextStyle(
+                              color: Colors.redAccent,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 15,
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
