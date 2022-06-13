876c6b3636a747f0e8232c92bbe87ddf867334bf
ayushshrestha1892 <ayushshrestha96@gmail.com>
2022-06-13 09:38:21 +0545

    profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fitness_app/models/user_model.dart';
+import 'package:fitness_app/screens/home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -8,8 +14,84 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  User? user = FirebaseAuth.instance.currentUser;
+
+  UserModel loggedInUser = UserModel();
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 180,
+            child: Icon(
+              Icons.account_circle,
+              size: 180,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            " Name: ${loggedInUser.firstName} ${loggedInUser.lastName}",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            " E-mail: ${loggedInUser.email}",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            " Age: ${loggedInUser.age}",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            " Wieght: ${loggedInUser.weight}kg",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            " Hieght: ${loggedInUser.height}cm",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+        ],
+      ),
+    ));
   }
 }
